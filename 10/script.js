6ca7791cbc3c647a262f59e1f12fd75e05142f8a
viveksharmaui <sharma_vivek62@yahoo.com>
2022-02-13 22:09:26 +0500

    react.js with internals

---
+++ 10/script.js
@@ -0,0 +1,15 @@
+// console.log(a);
+// var a = 123;
+
+// myFunc();
+// function myFunc() {
+
+// }
+
+// sync/async
+
+console.log("hello world 1");
+
+setTimeout(() => console.log("hello world 2"), 0);
+
+console.log("hello world 3");
---
+++ 10/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyFile from "./MyFile";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello world from App.js</h1>
+      <MyFile />
+    </main>
+  );
+}
+
+export default App;
