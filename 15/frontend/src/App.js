01e35e57c53c9e6ebdccc2f198b7ec6db70463d2
viveksharmaui <sharma_vivek62@yahoo.com>
2022-03-19 21:09:57 +0500

    MERN Stack app

---
+++ 15/frontend/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+
+import logo from './logo.svg';
+import './App.css';
+
+import axios from "axios";
+
+function App() {
+
+  useEffect(() => {
+    axios.get("http://localhost:4000").then((response) => {
+      console.log("GET", response);
+    }).catch((e) => {
+      console.log("GET", e.response)
+    });
+
+    axios.post("http://localhost:4000", {
+      name: "vivek"
+    }).then((response) => {
+      console.log("POST", response);
+    }).catch((e) => {
+      console.log("POST", e.response)
+    });;
+
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
